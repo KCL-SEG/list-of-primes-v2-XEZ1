60b30005024ec2c33973e416691fa27c26b82e03
XEZ1 <xxxaclockworkorangegmail.com>
2022-10-02 04:49:56 +0100

    Solve prime numbers problem weith throwing ValueError exception

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    for primeNumber in range(100000):
+        if primeNumber > 1:
+            for i in range(2, primeNumber):
+                if (primeNumber % i) == 0:
+                    break
+            else:
+                list.append(primeNumber)
+                if len(list) == number_of_primes:
+                    break
+
     return list
